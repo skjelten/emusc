4814609a2b948d06616b32e1bf2a59f4d45d8869
Håkon Skjelten <skjelten@pvv.org>
2022-03-06 00:42:14 +0100

    Updated MIDI Core class to use the same RAW MIDI decoder as used for the Win32 class + some minor fixes

--- src/midi_input_core.cc
+++ src/midi_input_core.cc
@@ -24,13 +24,14 @@
 #include "midi_input_core.h"
 
 #include <iostream>
+#include <vector>
 
 
 MidiInputCore::MidiInputCore(Synth *synth)
   : _synth(synth)
 {
   OSStatus ret;
-  // create client and ports
+  // Create client and ports
   MIDIClientRef client;
   ret = MIDIClientCreate(CFSTR("EmuSC"), NULL, NULL, &client);
   if (ret != noErr)
@@ -44,24 +45,27 @@ MidiInputCore::MidiInputCore(Synth *synth)
     
   // Open connections from all sources. TODO: How to specify source?
   int n = MIDIGetNumberOfSources();
-//  std::cout << n << " sources found" << std::endl;
+
+  std::vector<const char *> names;
   for (int i = 0; i < n; ++i) {
     MIDIEndpointRef src = MIDIGetSource(i);
-    MIDIPortConnectSource(inPort, src, NULL);
+
+    if (src) {
+      CFStringRef name = NULL;
+      MIDIObjectGetStringProperty(src, kMIDIPropertyName, &name);
+      CFStringEncoding enc = CFStringGetSystemEncoding();
+      names.push_back(CFStringGetCStringPtr(name, enc));
+
+      MIDIPortConnectSource(inPort, src, NULL);
+    }
   }
 
-
-
-  /*
-for(std::vector<unsigned int>::iterator it = sources.begin(); it != sources.end(); it++)
-  {
-    MIDIEndpointRef src = MIDIGetSource(*it);
-    std::cout << "Using source " << *it << " (" << sourceName(src) << ", " << sourceModel(src) << ", " << sourceManufacturer(src) << ")" << std::endl;
-    MIDIPortConnectSource(_midi_in, src, 0);
-  }
-  */
+  if (names.empty())
+    throw(Ex(-1, "No valid MIDI source endpoints to connect to"));
 
   std::cout << "EmuSC: Core MIDI input initialized" << std::endl;
+  for (auto n : names)
+    std::cout << " -> " << n << std::endl;
 }
 
 
@@ -82,67 +86,8 @@ void MidiInputCore::_midi_callback(const MIDIPacketList* packetList,
     length = packet->length;
     data = packet->data;
 
-    if ((data[0] >> 4) == 0x08) {                           // Note Off
-      midiEvent.type = se_NoteOff;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
+    send_raw_std_msg(_synth, data[0], data[1], data[2]);
 
-    } else if ((data[0] >> 4) == 0x09) {                    // Note On
-      midiEvent.type = se_NoteOn;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-
-    } else if ((data[0] >> 4) == 0x0a) {                    // Key pressure
-      midiEvent.type = se_KeyPressure;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-
-    } else if ((data[0] >> 4) == 0x0b) {                    // Control change
-      midiEvent.type = se_CtrlChange;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-
-    } else if ((data[0] >> 4) == 0x0c) {                    // Program change
-      midiEvent.type = se_PrgChange;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-
-    } else if ((data[0] >> 4) == 0x0d) {                    // Channel pressure
-      midiEvent.type = se_ChPressure;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-
-    } else if ((data[0] >> 4) == 0x0e) {                    // Pitch bend
-      midiEvent.type = se_PitchBend;
-      midiEvent.channel = data[0] & 0x0f;
-      midiEvent.data1 = data[1];
-      midiEvent.data2 = data[2];
-      midiEvent.status = 0xff;
-      _synth->midi_input(&midiEvent);
-    }
-
-    if (0)
-      std::cout << "D0=0x" << std::hex << (int) data[0]
-		<< " D1=0x" << (int) data[1]
-		<< " D2=0x" << (int) data[2] << std::endl;
     packet = MIDIPacketNext(packet);
   }
 }
@@ -152,21 +97,12 @@ void MidiInputCore::_midi_callback(const MIDIPacketList* packetList,
 
 void MidiInputCore::stop(void)
 {}
-  
+
+
 void MidiInputCore::run(Synth *synth)
 {}
-/*
-void MidiInput::receivePacket(const MIDIPacket* packet)
-{
-	std::vector<unsigned char> data;
-	for(unsigned int i = 0; i < packet->length; i++)
-		data.push_back(static_cast<unsigned char>(packet->data[i]));
-	std::copy(data.begin(), data.end(), std::ostream_iterator<int>(std::cout, " "));
-	std::cout << std::endl;
-}
 
-}
-*/
+
 void MidiInputCore::midi_callback(const MIDIPacketList* packetList,
 				  void* midiInput,
 				  void* srcConnRefCon)
